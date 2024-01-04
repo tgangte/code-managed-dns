c0b933f1bfc13847252d491e1c667b7281687261
tgangte <songangte29@gmail.com>
2024-01-03 19:20:20 -0800

    Terraform cloud migration (#7)
    * tfstate temp
    
    * Added second provider cloudflare and used tf cloud
    
    * A state file was deleted
    
    * Added required version

--- providers.tf
+++ providers.tf
@@ -1,15 +1,32 @@
 terraform {
   required_providers {
     ns1 = {
-      source = "ns1-terraform/ns1"
+      source  = "ns1-terraform/ns1"
       version = "~> 2.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
     }
   }
   required_version = ">= 0.13"
 }
 
 provider "ns1" {
-  apikey = var.ns1_apikey
+  apikey                 = var.ns1_apikey
   rate_limit_parallelism = 60
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+terraform {
+  cloud {
+    organization = "cabbage-patch"
+    workspaces {
+      name = "code-managed-dns-workspace"
+    }
+  }
 }
 
--- variables.tf
+++ variables.tf
@@ -1,15 +1,26 @@
-variable  ns1_apikey {
-default = "export TF_VAR_ns1_apikey=xxx"
-type = string
+variable "ns1_apikey" {
+  default = "export TF_VAR_ns1_apikey=xxxx"
+  type    = string
 }
 
-variable simple_records_under_zone {
-    description = "List of domains with their respective TTLs, record types, and record values to create as DNS records"
-    type     = list(object({
-    zone = string
-    domain = string
-    ttl    = number
-    record_type = string
+variable "cloudflare_api_token" {
+  default = "export cloudflare_api_token=xxxx"
+  type    = string
+}
+
+variable "cloudflare_zone_id" {
+  default     = "8b94f00b3d750fe8bab6ced25d960295"
+  type        = string
+  description = "Zone ID is the hash of the zone string by cloudflare. It is not a secret"
+
+}
+variable "simple_records_under_zone" {
+  description = "List of domains with their respective TTLs, record types, and record values to create as DNS records"
+  type = list(object({
+    zone         = string
+    domain       = string
+    ttl          = number
+    record_type  = string
     record_value = string
   }))
 
